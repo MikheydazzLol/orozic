c4a22d77cf70d77a7ce5e18762d150f279729092
MikheydazzLol <155878921+MikheydazzLol@users.noreply.github.com>
2024-02-12 18:37:39 +0700

    Add files via upload

---
+++ js/youtubes.js
@@ -0,0 +1,7 @@
+const link_items = document.getElementById('main_links');
+const youtube_items = document.getElementById('youtube_links');
+
+function ShowYoutube(){
+    link_items.classList.toggle('hide_links');
+    youtube_items.classList.toggle('hide_links');
+}
